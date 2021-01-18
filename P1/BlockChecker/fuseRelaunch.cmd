d4434d1972e80b7ef4c43a63eb69126e8e3ad6ae
Wuzimeng <74315048+Wuzimeng@users.noreply.github.com>
2021-01-18 23:50:26 +0800

    Add files via upload

---
+++ P1/BlockChecker/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/CO/P1/BlockChecker/BlockChecker_tb_isim_beh.exe" -prj "D:/CO/P1/BlockChecker/BlockChecker_tb_beh.prj" "work.BlockChecker_tb" "work.glbl" 
